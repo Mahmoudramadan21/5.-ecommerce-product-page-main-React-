6b1d02ac06a3d3581fcbcaaf644af8cd2cdd4da6
Mahmoudramadan21 <106267261+Mahmoudramadan21@users.noreply.github.com>
2024-10-20 21:28:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import '@fontsource/kumbh-sans/400.css';
-import '@fontsource/kumbh-sans/500.css';
-import '@fontsource/kumbh-sans/600.css';
 import '@fontsource/kumbh-sans/700.css';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
 
